f62e92bd4ff9b69cd2a2d49ded1965fd6459f43b
Jy0tiPal <jyoti.pal@sunbeaminfo.com>
2025-08-19 12:34:01 +0530

    Login added

--- staffPortalScEntry/src/App.jsx
+++ staffPortalScEntry/src/App.jsx
@@ -1,14 +1,25 @@
 import Footer from "./components/Footer";
 import Header from "./screens/navbar";
+import LogIn from "./screens/LogIn";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 function App()
 {
   return(
-    <>
-    <Header/>
-    <Footer/>
-    </>
+  <>
+    {/* <Header/> */}
+    <Routes>
+      <Route path="/" element={<LogIn />} />
+    </Routes> 
+    {/* <Footer/> */}
+    <ToastContainer />
+
+  </>
   );
 }
 
