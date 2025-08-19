4ef8ecbd804da70c690c28b40b4d5b864cdc0aca
Priyanka.Rangole <144417606+PriyankaRangole02@users.noreply.github.com>
2025-08-19 06:59:21 +0000

    dashboard with course menu item added

--- staffPortalScEntry/src/App.jsx
+++ staffPortalScEntry/src/App.jsx
@@ -4,7 +4,8 @@ import Dashboard from "./components/Dashboard";
 import Footer from "./components/Footer";
 
 import Header from "./screens/navbar";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App()
 {
